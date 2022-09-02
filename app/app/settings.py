36a405a1500797f5d94b1701c4cc4a439407c5e2
Odnoiko <dimaodnoiko@gmail.com>
2022-09-02 11:45:19 +0300

    Moved drf settings to settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -139,4 +139,13 @@ AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
